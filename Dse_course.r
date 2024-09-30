98255a437544b17c85a140a87505a1ab5f57c4e7
depa-tto <antonio.depatto@studenti.unimi.it>
2024-09-30 18:32:45 +0200

    lecture_2

--- Dse_course.r
+++ Dse_course.r
@@ -46,3 +46,62 @@ typeof(z)
 Re(z) # real part
 Im(z) # imaginary part
 Mod(z) # modulus
+
+# logical type
+
+x <- 2 > 1
+x
+
+# logical operations
+
+u <- TRUE
+v <- FALSE
+u & v
+
+address <- 'Via'
+domain <- 'Conservatorio'
+paste(address, domain, sep = ' ') # two character values can be concatenated with the paste function
+
+my_str = 'Via Conservatorio'
+sub('Via', 'Piazza', my_str) # with sub we can substitute terms in a string
+
+# vectors
+
+c(1,2,3,4,5,6)
+
+c("Italy","Spain","France","UK","Ireland","Belgium")
+
+# vectors can contain only equal data types
+
+c(FALSE, 1, '2') # in this case, FALSE and 1 will be converted to characters
+
+x <- c(FALSE, 1, '2') 
+class(x[1])
+class(x[2])
+class(x[3])
+
+# vectors with attached labels
+
+c('Tottenham' = 14, 'Aston Villa' = 12, 'Brentford' = 6)
+
+x <- c(14,12,6) # vector
+n <- c('Tottenham','Aston Villa','Brentford') # vector of names
+names(x) <- n # assigning names
+x
+
+# matrix
+
+M <- matrix(data = c(1,2,3,4,5,6), # data elements
+        ncol = 3, # number of columns
+        nrow = 2, # number of rows
+        byrow = TRUE) # fill matrix by rows
+
+M
+
+rn <- c('row1', 'row2')
+cn <- c('col1', 'col2', 'col3')
+
+rownames(M) <- rn
+colnames(M) <- cn
+
+M
